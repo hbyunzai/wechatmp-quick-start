c3ae36eff0157f1fe78bedb13929e0f1e02d2c68
ferried <harlancui@Outlook.com>
2020-02-03 11:45:03 +0800

    :tada: init
    init

---
+++ src/main/java/com/github/hbyunzai/wechat/quickstart/DemoApplication.java
@@ -0,0 +1,23 @@
+/*
+ * @Date: 2020-02-03 11:29:16
+ * @Author: ferried
+ * @Email: harlancui@outlook.com
+ * @LastEditors: ferried
+ * @LastEditTime: 2020-02-03 11:42:47
+ * @Editor: Visual Studio Code
+ * @Desc: nil
+ * @License: nil
+ */
+package com.github.hbyunzai.wechat.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hbyunzai/wechat/quickstart/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hbyunzai.wechat.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
